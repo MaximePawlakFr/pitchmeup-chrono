c67c66f8746df4d23e3c212ddb18be5932b15ed8
Maxime Pawlak <contact@maximepawlak.fr>
2018-08-17 20:44:14 +0200

    Refactor NetworkPanel.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Digits from "./Digits";
 import DigitForm from "./DigitForm";
 import Time from "./Time";
 import FirebaseHelper from "./FirebaseHelper";
+import NetworkPanel from "./NetworkPanel";
 
 class App extends Component {
   constructor() {
@@ -13,7 +14,7 @@ class App extends Component {
       minutes: 5,
       seconds: 0,
       status: "disconnected",
-      master:null
+      master: null
     };
     this.timer = this.timer.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -21,15 +22,12 @@ class App extends Component {
     this.handleDigitsChange = this.handleDigitsChange.bind(this);
     this.handleStop = this.handleStop.bind(this);
     this.handleConnect = this.handleConnect.bind(this);
-    this.chronoNameInput = React.createRef();
-    this.masterNameInput = React.createRef();
-    this.masterPasswordInput = React.createRef();
+
     this.reset = this.reset.bind(this);
     this.start = this.start.bind(this);
     this.handleDisconnect = this.handleDisconnect.bind(this);
     this.disconnect = this.disconnect.bind(this);
-    this.startFromStore = this.startFromStore.bind(this);
-    this.handleConnectMaster = this.handleConnectMaster.bind(this);
+    this.handleSetupMaster = this.handleSetupMaster.bind(this);
     this.unsubscribe = null;
   }
 
@@ -59,20 +57,18 @@ class App extends Component {
   }
 
   handleFormSubmit(duration) {
-    console.log(duration);
-    
-    this.handleStart({duration});
+    this.handleStart({ duration });
   }
 
-  async start({duration, startAt}){
-    console.log(duration,startAt);
-    
+  async start({ duration, startAt }) {
+    console.log(duration, startAt);
+
     const now = await Time.getUTCTime();
     const localNow = Date.now();
     const diffTime = localNow - now;
 
-    if(this.state.master && this.state.master.isActive){
-      FirebaseHelper.createChrono(this.state.master.name, startAt || now , duration, this.state.master.password)
+    if (this.state.master && this.state.master.isActive) {
+      FirebaseHelper.createChrono(this.state.master.name, startAt || now, duration, this.state.master.password)
     }
 
     this.countdown = setInterval(() => {
@@ -82,63 +78,52 @@ class App extends Component {
     this.setState({ startAt: startAt || now, diffTime });
   }
 
-  handleStart(duration) {
-    this.handleStop()
-    this.start(duration);
+  handleStart(data) {
+    let duration = data.duration;
+    let startAt = null;
+    if (data && data.error) {
+      this.setState({ status: `no chrono found '${data.name}'` });
+      return;
+    } else if (data && data.public) {
+      startAt =
+        data.public.startAt.seconds * 1000 +
+        data.public.startAt.nanoseconds / 1000;
+      duration = data.public.duration;
+    }
+    this.handleStop();
+    this.start({ duration, startAt });
   }
 
   handleStop() {
     this.countdown = clearInterval(this.countdown);
   }
 
-   startFromStore(data) {
-    console.log("startFromStore", data);
-    if (data && data.error) {
-      this.setState({ status: `no chrono found '${data.name}'` });
-      return;
-    }
-    const startAt =
-      data.public.startAt.seconds * 1000 +
-      data.public.startAt.nanoseconds / 1000;
-    const duration = data.public.duration;
-    this.handleStop();
-    this.start({duration, startAt});
-  }
-
-  handleConnect(event) {
-    event.preventDefault();
-    this.reset();
-
-    const name = this.chronoNameInput.current.value;
-    console.log("Connect: ", name);
-    this.unsubscribe = FirebaseHelper.findChrono(name, this.startFromStore);
-    this.setState({ status: `connected to '${name}'` });
-
-  }
-
-  handleConnectMaster(event) {
-    event.preventDefault();
-    this.reset();
-
-    const name = this.masterNameInput.current.value;
-    const password = this.masterPasswordInput.current.value;
-
-    FirebaseHelper.createChrono(name, Date.now(), 3600, password);
-    this.unsubscribe = FirebaseHelper.findChrono(name, this.startFromStore);
-
-    this.setState({ status: `mastering '${name}'`, master:{isActive:true, name, password} });
-  }
-
-
   handleDisconnect() {
     this.reset();
-   
   }
 
-  disconnect(){
+  handleConnect(name) {
+    this.reset();
+
+    console.log("Connect: ", name);
+    this.unsubscribe = FirebaseHelper.findChrono(name, this.handleStart);
+    this.setState({ status: `connected to '${name}'` });
+  }
+
+  async handleSetupMaster(name, duration, password) {
+    this.reset();
+    const startAt = await Time.getUTCTime();
+    FirebaseHelper.createChrono(name, startAt, duration, password);
+    this.unsubscribe = FirebaseHelper.findChrono(name, this.handleStart);
+
+    this.setState({ status: `mastering '${name}'`, master: { isActive: true, name, duration, password } });
+  }
+
+
+  disconnect() {
     if (this.unsubscribe) {
       this.unsubscribe();
-      this.setState({ status: "disconnected", master:null });
+      this.setState({ status: "disconnected", master: null });
     }
   }
 
@@ -165,31 +150,9 @@ class App extends Component {
             seconds={0}
           />
         </div>
-        <h2>Status : {this.state.status}</h2>
-        <button role="button" onClick={this.handleDisconnect}>Disconnect</button>
-        <div>
-          <form onSubmit={this.handleConnect}>
-            <input
-              type="text"
-              ref={this.chronoNameInput}
-              placeholder="Chrono name..."
-            />
-            <button type="submit">Connect</button>
-          </form>
-          <form onSubmit={this.handleConnectMaster}>
-            <input
-              type="text"
-              placeholder="Chrono name..."
-              ref={this.masterNameInput}
-            />
-            <input
-              type="password"
-              placeholder="master password"
-              ref={this.masterPasswordInput}
-            />
-            <button type="submit">Master a new chrono</button>
-          </form>
-        </div>
+        Status: {this.state.status}
+        <NetworkPanel onConnect={this.handleConnect} onDisconnect={this.handleDisconnect} onSetupMaster={this.handleSetupMaster} />
+
       </div>
     );
   }
---
+++ src/NetworkPanel.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+export default class NetworkPanel extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      status: 'DISCONNECTED'
+    }
+
+    this.handleConnect = this.handleConnect.bind(this);
+    this.handleDisconnect = this.handleDisconnect.bind(this);
+    this.handleSetupMaster = this.handleSetupMaster.bind(this);
+
+    this.chronoNameInput = React.createRef();
+    this.masterNameInput = React.createRef();
+    this.masterPasswordInput = React.createRef();
+    this.minutesInput = React.createRef();
+    this.secondsInput = React.createRef();
+  }
+
+
+
+  handleConnect(event) {
+    event.preventDefault();
+    const name = this.chronoNameInput.current.value;
+    this.setState({ status: "CONNECTED" });
+    this.props.onConnect(name);
+  }
+
+  handleSetupMaster(event) {
+    event.preventDefault();
+
+    const name = this.masterNameInput.current.value;
+    const password = this.masterPasswordInput.current.value;
+    const minutes = parseInt(this.minutesInput.current.value || 0, 10);
+    const seconds = parseInt(this.secondsInput.current.value || 0, 10);
+    const duration = minutes * 60 + seconds;
+    this.setState({ status: "CONNECTED" });
+    this.props.onSetupMaster(name, duration, password)
+  }
+
+
+  handleDisconnect() {
+    this.setState({ status: "DISCONNECTED" });
+    this.props.onDisconnect();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Network</h2>
+        {this.state.status !== "DISCONNECTED" ? (<button onClick={this.handleDisconnect}>Disconnect</button>
+
+        ) : (
+            <div>
+              <form onSubmit={this.handleConnect}>
+                <h4>Connect to a chrono</h4>
+                <input
+                  type="text"
+                  ref={this.chronoNameInput}
+                  placeholder="Chrono name..."
+                />
+                <button type="submit">Connect</button>
+              </form>
+              <form onSubmit={this.handleSetupMaster}>
+                <h4>Setup a master chrono</h4>
+                <input
+                  type="number"
+                  name="minutes"
+                  placeholder="minutes"
+                  min="0"
+                  max="59"
+                  ref={this.minutesInput}
+                />
+                :<input
+                  type="number"
+                  name="seconds"
+                  placeholder="seconds"
+                  min="0"
+                  max="59"
+                  ref={this.secondsInput}
+                />
+
+
+                <input
+                  type="text"
+                  placeholder="Chrono name..."
+                  ref={this.masterNameInput}
+                />
+                <input
+                  type="password"
+                  placeholder="master password"
+                  ref={this.masterPasswordInput}
+                />
+                <button type="submit">Master a new chrono</button>
+              </form>
+            </div>
+          )}
+      </div>
+    )
+  }
+}
