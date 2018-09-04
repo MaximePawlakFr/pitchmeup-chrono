db775c1c70b67dd84ea9d4095af650e8065cdbd6
Maxime Pawlak <contact@maximepawlak.fr>
2018-09-04 10:25:35 +0200

    Add timestamp when building app.

--- src/Version.js
+++ src/Version.js
@@ -1,8 +1,9 @@
 import React from "react";
 import versionNumber from "./versionNumber";
+import timestamp from "./timestamp";
 
 const getVersionNumber = ({ major, minor, patch }) => {
-  return [major, minor, patch].join(".");
+  return [major, minor, patch].join(".") + "-" + timestamp;
 };
 
 export default () => {
