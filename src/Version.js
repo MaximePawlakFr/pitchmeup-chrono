d3975c7a7785210fe02a331221176400542696f8
Maxime Pawlak <contact@maximepawlak.fr>
2018-09-09 19:03:37 +0200

    Add css to Version.js

--- src/Version.js
+++ src/Version.js
@@ -1,11 +1,12 @@
 import React from "react";
 import versionNumber from "./versionNumber";
 import timestamp from "./timestamp";
+import "./Version.css";
 
 const getVersionNumber = ({ major, minor, patch }) => {
   return [major, minor, patch].join(".") + "-" + timestamp;
 };
 
 export default () => {
-  return <div>v{getVersionNumber(versionNumber)}</div>;
+  return <div class="versionInfos">v{getVersionNumber(versionNumber)}</div>;
 };
