fd51d271275442ab459f64a135120c3fcc5da5d9
Maxime Pawlak <contact@maximepawlak.fr>
2018-09-23 18:15:00 +0200

    Delete useless files.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import FirebaseHelper from "./utils/FirebaseHelper";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
 FirebaseHelper.init();
